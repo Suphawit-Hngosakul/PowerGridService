88e22fb96dcd65e7e86bab66f0bdfb3e6ab3b0d8
Suphawit Hngosakul - 6609650053 <145352874+Suphawit-Hngosakul@users.noreply.github.com>
2026-05-03 16:19:19 +0700

    reset

--- main/infra/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "api_url" {
-  description = "API Gateway URL"
-  value       = "https://${aws_api_gateway_rest_api.powergrid_api.id}.execute-api.${var.aws_region}.amazonaws.com/prod"
-}
-
-output "rds_endpoint" {
-  description = "RDS Endpoint"
-  value       = aws_db_instance.powergrid_db.address
-}
